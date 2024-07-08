fd4c6e29d93eb45ea92c379dcca28227d179f22e
Saliner Sidhu <salinder.sid@gmail.com>
2024-07-07 21:56:22 -0400

    feat(header): added header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,20 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/header";
+import { HeaderLinks } from "@/data/headerLinks";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Quakemap | Realtime Earthquakes",
-  description: "Track earthquakes around the world in realtime from the comfort of your smartphone.",
+  description:
+    "Track earthquakes around the world in realtime from the comfort of your smartphone.",
   openGraph: {
     siteName: "Quakemap",
     title: "Quakemap",
-    type: "website"
-  }
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -21,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header links={HeaderLinks} />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/data/headerLinks.tsx
@@ -0,0 +1,10 @@
+export type HeaderLink = {
+  name: string;
+  href: string;
+};
+
+export const HeaderLinks: HeaderLink[] = [
+  { name: "FAQ", href: "/faq" },
+  { name: "EULA", href: "/eula" },
+  { name: "Privacy", href: "/privacy" },
+];
