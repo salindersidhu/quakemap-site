2c7a2df23c009ad3a3476e1db499642a59cc5575
Saliner Sidhu <salinder.sid@gmail.com>
2024-07-08 00:17:44 -0400

    feat(404): add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main>
+      <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8 hero-section">
+        <div className="max-w-max mx-auto">
+          <main className="sm:flex">
+            <p className="text-4xl font-extrabold text-gray-900 sm:text-5xl">
+              404
+            </p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  Page Not Found
+                </h1>
+                <p className="mt-4 text-base text-gray-500">
+                  Please check the URL and try again.
+                </p>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <Link
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-900 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  href="/"
+                >
+                  Home
+                </Link>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </main>
+  );
+}
