d97bb6dd6881e7bec1df482361a5d53504baf5c6
Saliner Sidhu <salinder.sid@gmail.com>
2024-07-08 21:06:44 -0400

    refactor(manifest): Fixed icons in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,9 +9,14 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
     theme_color: "#fff",
