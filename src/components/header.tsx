8d91a7e019f7416ab4ea7e650643c2f8d38219af
Saliner Sidhu <salinder.sid@gmail.com>
2024-07-13 00:42:01 -0400

    feat(darkMode): add dark theme for header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,15 +8,16 @@ import {
   DisclosurePanel,
 } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import ThemeSwitch from "./themeSwitch";
 
 export default function Header({ links }: { links: NavigationLink[] }) {
   return (
-    <Disclosure as="nav" className="bg-gray-900">
+    <Disclosure as="nav" className="bg-gray-100 dark:bg-gray-900">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-950 dark:text-gray-200 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
@@ -34,9 +35,9 @@ export default function Header({ links }: { links: NavigationLink[] }) {
               <Link href="/">
                 <span className="sr-only">Quakemap</span>
                 <Image
-                  src="/logo_white.svg"
+                  src="/logo.svg"
                   alt=""
-                  className="h-8 w-auto"
+                  className="h-8 w-auto dark:invert"
                   height={32}
                   width={32}
                 />
@@ -48,13 +49,16 @@ export default function Header({ links }: { links: NavigationLink[] }) {
                   <Link
                     key={item.name}
                     href={item.href}
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-base text-gray-950 dark:text-gray-200 hover:bg-white dark:hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium"
                   >
                     {item.name}
                   </Link>
                 ))}
               </div>
             </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <ThemeSwitch />
           </div>
         </div>
       </div>
@@ -66,7 +70,7 @@ export default function Header({ links }: { links: NavigationLink[] }) {
               key={item.name}
               as={Link}
               href={item.href}
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+              className="text-gray-950 dark:text-gray-200  hover:bg-white dark:hover:bg-gray-700 block rounded-md px-3 py-2 text-base font-medium"
             >
               {item.name}
             </DisclosureButton>
