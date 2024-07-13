f4f7fb72e34a249a911d0725943d0bb517f704f6
Saliner Sidhu <salinder.sid@gmail.com>
2024-07-13 00:24:59 -0400

    refactor(pages): update styles and layout on FAQ, EULA and Privacy Policy pages

--- src/app/eula/page.tsx
+++ src/app/eula/page.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
 export default function EULA() {
   return (
     <main>
-      <div className="bg-white px-6 py-32 lg:px-8">
+      <div className="bg-white px-6 py-6 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-          <h1 className="mt-2 text-center font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h1 className="mt-2 text-center font-bold tracking-tight text-gray-900 text-2xl sm:text-4xl">
             End-User License Agreement
           </h1>
           <div className="mt-10">
--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -3,8 +3,8 @@ import type { Faq } from "@/data/faq";
 export default function Faq({ faqs }: { faqs: Faq[] }) {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-7xl divide-y divide-gray-900/10 px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
-        <h1 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+      <div className="mx-auto max-w-7xl divide-y divide-gray-900/10 px-6 py-6 sm:py-32 lg:px-8">
+        <h1 className="text-2xl sm:text-4xl font-bold leading-10 tracking-tight text-gray-900">
           Frequently Asked Questions
         </h1>
         <dl className="mt-10 space-y-8 divide-y divide-gray-900/10">
