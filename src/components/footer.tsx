a724ebe5cb1a638cb2015e37dad29c939b7e4f5c
Saliner Sidhu <salinder.sid@gmail.com>
2024-07-13 00:58:26 -0400

    feat(darkMode): add dark mode to the Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer({
   socials: SocialLink[];
 }) {
   return (
-    <footer className="bg-white">
+    <footer className="bg-white dark:bg-zinc-950">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
@@ -20,7 +20,7 @@ export default function Footer({
           {links.map((item) => (
             <div key={item.name} className="px-5 py-2">
               <Link
-                className="text-base text-gray-500 hover:text-gray-900"
+                className="text-base text-gray-950 dark:text-gray-200 hover:text-gray-400 hover:dark:text-white"
                 href={item.href}
               >
                 {item.name}
@@ -34,7 +34,7 @@ export default function Footer({
               key={`${item.name}-${index}`}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-400 hover:text-gray-500"
+              className="text-gray-950 dark:text-gray-200 hover:text-gray-400 hover:dark:text-white"
               href={item.href}
             >
               <span className="sr-only">{item.name}</span>
@@ -42,7 +42,7 @@ export default function Footer({
             </Link>
           ))}
         </div>
-        <p className="mt-8 text-center text-base text-gray-500">
+        <p className="text-base mt-8 text-center text-gray-950 dark:text-gray-200">
           &copy; {new Date().getFullYear()} Salinder Sidhu. All rights reserved.{" "}
         </p>
       </div>
